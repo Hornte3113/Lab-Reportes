0622adb35f07deffef1b3cc45f6ac9a60383f1c2
Hornte3113 <243777@ids.upchiapas.edu.mx>
2026-01-31 01:07:07 -0600

    Añadi otra view de los productos mñas vendidos

--- db/reports_vw.sql
+++ db/reports_vw.sql
@@ -40,4 +40,29 @@ GROUP BY vd.categoria_id, vd.categoria, t.gran_total
 ORDER BY ingresos_totales DESC;
 
 
----view 2
+---view 2 Top de Prodcutos
+--devuelve el ranking de de productos más vendidos
+
+CREATE OR REPLACE VIEW view_top_productos AS
+SELECT 
+p.id as producto_id,
+c.nombre as categoria,
+COALESCE(SUM(od.cantidad), 0) as  unidades_vendidas,
+COALESCE(SUM(od.subtotal) 0) as ingresos_generados,
+COUNT(DISTINCT od.orden_id) as ordenes_incluido,
+p.precio as precio_actual,
+p.stock as stock_actual,
+
+ROW_NUMBER() OVER (ORDER BY COALESCE(SUM(od.cantidad), 0) DESC) as ranking_ventas,
+ROW_NUMBER() OVER (ORDER BY COALESCE(SUM(od.subtotal), 0) DESC) as ranking_ingresos,
+ROUND(
+    100.0 * SUM(COALESCE(od.subtotal, 0)) OVER (ORDER BY COALESCE(SUM(od.subtotal), 0)DESC) / 
+    NULLIF(SUM(COALESCE(od.subtotal, 0)) OVER (), 0), 2
+) as pct_acumulado
+FROM productos p 
+INNER JOIN categorias c ON p.categoria_id = c.id
+LEFT JOIN orden_detalles od ON p.id = od.producto_id
+LEFT JOIN ordenes o ON od.orden_id = o.id AND o.status !=  'cancelado'
+GROUP BY p.id, p.codigo, p.nombre, c.nombre, p.precio, p.stock
+HAVING COALESCE(SU(od.cantidad), 0) > 0
+ORDER BY unidades_vendidas DESC;
